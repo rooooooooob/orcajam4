615418c301aac880f44c9d6f1c942f43aede5c8f
rooooooooob <raftias@gmail.com>
2013-09-07 15:39:41 -0700

    added tile modifying ability to facilitate easier level editing

--- src/World.cpp
+++ src/World.cpp
@@ -6,9 +6,9 @@ namespace orca
 {
 
 World::World(je::Game * const game)
-	:Level(game, "orcajam4/levels/world.tmx")
+	:Level(game, "orcajam4/levels/tiny.tmx")
 {
-	this->addEntity(new Player(this, sf::Vector2f(320 - 8, 240 - 8)));
+	this->addEntity(new Player(this, sf::Vector2f(320, 240)));//186 * 16 - 8, 151 * 16 - 8)));
 }
 
 void World::onDraw(sf::RenderTarget& target) const
@@ -17,6 +17,33 @@ void World::onDraw(sf::RenderTarget& target) const
 
 void World::loadEntities(const std::string& layerName, const std::vector<EntityPrototype>& prototypes)
 {
+}
+
+void World::transformTiles(const std::string& layerName, int tilesAcross, int tilesHigh, unsigned  **tiles)
+{
+	//	make a copy of the tiles to refer to while transforming
+	unsigned int **untransformed = new unsigned int*[tilesAcross];
+	for (int i = 0; i < tilesAcross; ++i)
+	{
+		untransformed[i] = new unsigned int[tilesHigh];
+		for (int j = 0; j < tilesHigh; ++j)
+			untransformed[i][j] = tiles[i][j];
+	}
+	/**		modify tiles here (this is your job Max)		**/
+	if (layerName == "terrain")
+	{
+		for (int x = 0; x < tilesAcross; ++x)
+		{
+			for (int y = 0; y < tilesAcross; ++y)
+			{
+
+			}
+		}
+	}
+
+	for (int i = 0; i < tilesAcross; ++i)
+		delete[] untransformed[i];
+	delete[] untransformed;
 }
 
 }
--- src/World.hpp
+++ src/World.hpp
@@ -13,6 +13,7 @@ public:
 private:
 	void onDraw(sf::RenderTarget& target) const;
 	void loadEntities(const std::string& layerName, const std::vector<EntityPrototype>& prototypes);
+	void transformTiles(const std::string& layerName, int tilesAcross, int tilesHigh, unsigned  **tiles) override;
 };
 
 }
