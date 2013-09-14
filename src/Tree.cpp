0b872cd60cdde55db911518165c9a596061415d2
rooooooooob <raftias@gmail.com>
2013-09-14 00:11:57 -0700

    fixed tree chopping so it actually works and animates properly

--- src/Tree.cpp
+++ src/Tree.cpp
@@ -10,7 +10,7 @@ namespace orca
 
 Tree::Tree(World * world, const sf::Vector2f& pos)
 	:Entity(world, "Tree", pos, sf::Vector2i(8, 8), sf::Vector2i(-4, -4)),
-	cutting (world->getGame().getTexManager().get("palm_tree2.png"), 32, 32, 1, false),
+	cutting(world->getGame().getTexManager().get("palm_tree2.png"), 64, 32, 1, false),
 	hp(50)
 {
 	//this->pos.y -= 16;
@@ -47,7 +47,7 @@ void Tree::draw(sf::RenderTarget& target) const
     cutting.draw(target);
 }
 
-void Tree::chop ()
+void Tree::chop()
 {
     hp -= 1;
     if (hp == 49 || hp == 25 || hp == 0)
